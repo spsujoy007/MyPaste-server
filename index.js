a3a660a24dc1d67e798adff2e7be1e228c9d8f64
Sujoy Paul <freelancersujoy@gmail.com>
2023-04-13 12:05:01 +0600

    copied count

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json())
 async function run () {
     try{
         const notesCollection = client.db('MyPaste').collection('notes');
+        const pinNotesCollection = client.db('MyPaste').collection('pinNotes');
         
         // add a new note 
         app.post('/addnote', async(req, res) => {
@@ -26,21 +27,83 @@ async function run () {
 
         //to get all notes
         app.get('/notes', async(req, res) => {
-            const notes = await notesCollection.find({}).sort({_id: -1}).toArray()
+            const email = req.query.email;
+            const query = {email: email}
+            const notes = await notesCollection.find(query).sort({_id: -1}).toArray()
             res.send(notes)
         })
 
         // get single note 
-        
+        app.get('/note/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await notesCollection.findOne(query);
+            res.send(result)
+        })
 
         // to delete an note 
         app.delete('/deletenote', async(req, res) => {
             const id = req.query.id;
-            console.log(id)
             const query = { _id: new ObjectId(id)}
             const result = await notesCollection.deleteOne(query);
             res.send(result)
         })
+
+        // to pin a note 
+        app.put('/pin', async(req, res) => {
+            const id = req.query.id;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) }
+            const options = {upsert: true}
+            const updatedDoc = {
+                $set: {
+                    pinned: true
+                }
+            }
+            const result = await notesCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        // get all pin notes
+        app.get('/pinnotes', async(req, res) => {
+            const email = req.query.email;
+            const filterEmail = {email: email, pinned: true}
+            const result = await notesCollection.find(filterEmail).toArray()
+            res.send(result)
+        })
+
+        // un pin a note
+        app.put('/removePin', async(req, res) => {
+            const id = req.query.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true}
+            const updatedDoc = {
+                $set: {
+                    pinned: false
+                }
+            }
+            const result = await notesCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        app.put('/copiedCount', async(req, res) => {
+            const id = req.query.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true}
+            
+            const mynote = await notesCollection.findOne(filter);
+            const previousCopyCount = mynote.copied_count;
+
+            const updatedDoc = {
+                $set: {
+                    copied_count: previousCopyCount + 1
+                }
+            }
+            const result = await notesCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
+
+
     }
     finally{
 
